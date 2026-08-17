34870f1018312ee03cb2f5c5592a4e737ede86de
rashelmahomud <rashelmahmudraj1998@gmail.com>
2026-08-17 19:59:28 +0600

    added some eiditing

--- next.config.ts
+++ next.config.ts
@@ -6,7 +6,11 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.pravatar.cc"
+        hostname: "i.pravatar.cc",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com"
       }
     ]
   }
